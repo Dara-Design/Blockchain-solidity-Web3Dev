af6ce2ac9a7beb1c0d6bb4d6ac91c38f686eebee
unknown <you@example.com>
2023-05-16 12:39:57 +0200

    Interacting with other contracts

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,26 +1,44 @@
 //SPDX-Licence-Identifier: MIT
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
-//you need to have compatible versions of different contracts
-
-//EVM -ETHEREUM VIRTUAL MACHINE
 
 
-//need to import contract we wonna build
+
+
+//need to import contact we wonna build
 import "./SimpleStorage.sol";
+
+
+//smart contracts are composable -they can easily interact with each other
+//we can have contracts deploying other contracts for us
+
 
 
 contract StorageFactory {
     //WE ARE CREATING A FUNCTION FOR DEPLOYING OF ANOTHER IMPORTED
     // CONTRACT AND SAVING IT TO THE GLOBAL VARIALBE
-     
-    SimpleStorage[] public simpleStorageArray;
-    //creating an array for deploying multiple contracts
 
+   SimpleStorage[] public simpleStorageArray;
+    //creating an array for deploying multiple contracts -pravimo niz koji nam omogucava da napravimo vise ugovora i sacuvamo u varijablu 
+
+    //funkcija za kreiranje ugovora iz drugog ugovora 
     function createSimpleStorageContract() public {
-
       SimpleStorage  simpleStorage = new SimpleStorage();
       simpleStorageArray.push(simpleStorage);
     }
 
-}
+  
+  
+  //we are creating a function that allows us to call store function from SimpleStorage contract
+
+   function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+    // za svaku interakciju sa ugovorom trebace ti dve stvari : Adresa ugovora i ABI -interface koji ti prikazuje kako  mozes da vrsis interakciju sa njim
+    //Adress
+    //ABI -showing us all inputs and= outputs
+    //kreiramo novi objekat koji sa nalazi u nizu simpleStorageArray na poziciji(indexu) _simpleStorageIndex
+    SimpleStorage simpleStorage =  SimpleStorage(simpleStorageArray[_simpleStorageIndex]);
+   // i iz tog objekta pozivamo funkciju store
+   simpleStorage.store(_simpleStorageNumber); //simpleStorageNumber cuvas u toj funkciju store iz drugog ugovora
+
+     }
+      }
